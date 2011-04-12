a39d6b43b7345f757afcf1767bf02fb590c787d5
Timothy Guan-tin Chien <timdream@gmail.com>
2011-04-12 12:37:17 +0800

    Add deploy script

---
+++ deploy/index.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Content-Type: text/plain');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+	print <<<EOL
+Error: Use POST request to update.
+For testing, use 'curl -d dummy=data'.
+EOL;
+	exit;
+}
+
+if (!file_exists('./config.inc.php')) {
+	print 'Error: config.inc.php not found.';
+	exit;
+}
+
+require('./config.inc.php');
+
+system('svn update ' . $root);
+
