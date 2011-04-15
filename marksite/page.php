8f7c8a14c2f40f660a2995a871cfd20cf1a9033a
Timothy Guan-tin Chien <timdream@gmail.com>
2011-04-15 15:42:21 +0800

    Add tracking code

--- marksite/page.php
+++ marksite/page.php
@@ -26,6 +26,19 @@ switch($this->current[0])
     <link rel="stylesheet" type="text/css" href="<?php echo $theme_assets_uri;?>style.css" />
     <link rel="shortcut icon" href="<?php echo $theme_assets_uri;?>favicon.ico" type="image/x-icon" />
     <meta property="og:image" content="<?php echo $theme_assets_uri;?>coscup.png" />
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-12923351-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 </head>
 <body>
 <div id="header">
