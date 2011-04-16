ca6a0af5a1fe438043d5fe49379c5b128af94194
Xaver Y.R. Chen <yrchen@ATCity.org>
2011-04-16 07:10:44 -0700

    Remove all unused tags

--- drupal/page.tpl.php
+++ drupal/page.tpl.php
@@ -5,158 +5,81 @@
 	<title><?php print $head_title; ?></title>
 
 <?php 	print $head;
-	print $assets;
-	print $styles;
-	print $scripts; ?>
+//	print $styles;
+//	print $scripts;
+?>
+<!-- COSCUP 2011 -->
 <link type="text/css" rel="stylesheet" media="all" href="http://coscup.org/2011-theme/assets/style.css" />
-</head><body class="<?php print $body_classes; ?>">
+<meta property="og:image" content="http://coscup.org/2011-theme/assets/coscup.png" />
+</head>
 
-
-
-
-
-
-
-
-
-
+<body class="<?php print $body_classes; ?>">
 <div id="header" class="header">
-
-<?php	if (!empty($primary_links) || $navigation_bar) { ?>
-	
-	<div class="nav">
-
-<?php print theme('links', $primary_links, array('id' => 'primary', 'class' => 'links main-menu'));?>
-<?php if ($navigation_bar) print $navigation_bar; ?>
-
+	<div class="info">
+		<h1><a href="#" title="首頁">COSCUP</a></h1>
+		<p id="title">開源人年會</p>
+		<p id="title_en">Conference for Open Source <span id="coders">Coders</span>, <span 
+id="users">Users</span> and <span id="promoters">Promoters</span></p>
+		<p id="date" title="2011 年 8 月 20 - 21 日">8/20 - 21, 2011 :: 台灣台北</p>
+		<div id="nav" class="empty">
+		<!-- 上面的 class="empty" 會觸發 script 拉遠端資料顯示 -->
+		</div>
+		<div id="language">
+			<ul>
+				<li><a href="#">English</a></li>
+				<li><a href="#">正體中文</a></li>
+				<li><a href="#">简体中文</a></li>
+			</ul>
+		</div>
+		<div id="message">
+			<p>Come on rock with <em>Gadgets beyond Smartphones</em>!</p>
+		</div>
+		<p id="mascot_icon"></p>
+		<div id="connect_box">
+			<ul>
+				<li><a title="加到 Google 日曆" target="_blank" 
+href="https://www.google.com/calendar/event?action=TEMPLATE&amp;text=COSCUP+2011&amp;dates=20110820T010000Z/20110821T100000Z&amp;details=http%3A%2F%2Fcoscup.org/2011/&amp;trp=true&amp;sprop=http%3A%2F%2Fcoscup.org/2011/&amp;sprop=name:COSCUP"><span 
+class="sprite gcal"></span></a></li>
+				<li><a title="Facebook 粉絲團" href="https://www.facebook.com/coscup" 
+target="_blank"><span class="sprite facebook"></span></a></li>
+				<li><a title="小啄在 Twitter" href="https://twitter.com/coscup" 
+target="_blank"><span class="sprite twitter"></span></a></li>
+				<li><a title="小啄在噗浪" href="http://www.plurk.com/coscup" 
+target="_blank"><span class="sprite plurk"></span></a></li>
+				<li><a title="部落格 RSS Feed" href="http://feeds.feedburner.com/coscup" 
+target="_blank"><span class="sprite rss"></span></a></li>
+			</ul>
+		</div>
 	</div>
-	
-<?php	} ?>
-
-<?php	if (!empty($logo)) { ?>
-	
-		<a id="coscupLogotype" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>"></a>
-		
-<?php	} ?>
-
-<?php	if (!empty($site_name)) { ?>
-	
-		<h1><a href="<?php print $front_page ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a></h1>
-		
-<?php	} ?>
-
-<?php	if (!empty($site_slogan)) { ?>
-	
-		<p><?php print $site_slogan; ?></p>
-		
-<?php	} ?>
-
-	<?php print $header_block; ?>
-
 </div>
-
-
-
-
-
-
-
-
-
 	
-<div class="section">
-
-	<div class="article">
-
-<?php	if ($content_top) { ?>
-		
-		<div class="header"><?php print $content_top; ?></div>
-		
-<?php	}  ?>
-					
-
-
-
-<?php	if ($tabs) { ?>
-				
-		<div class="nav tabs"><?php print $tabs; ?></div>
-		
-<?php	} ?>
-			
-
-
-
+<div id="content">
 
 <?php	print $content; ?>
 			
-
-
-
-
-<?php	if ($content_bottom) { ?>
-			
-		<div class="footer"><?php print $content_bottom; ?></div>
-			
-<?php 	} ?>
-
-	</div>
-	
-
-
-
-
-<?php	if ($sidebar) { ?>
-
-	<div class="aside">
-
-<?php	print $sidebar; ?>
-
-	</div>
-
-<?php	} ?>
-
 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<div class="footer">
-
-	<div class="section wrap">
-
-		<span><?php print $footer_message; ?></span>
-
-
-
-<?php print $footer_block; ?>
-	
+<div id="sidebar"> 
+	<div class="sponsors" class="empty"> 
+		<!-- 上面的 class="empty" 會觸發 script 拉遠端資料顯示 --> 
+	</div> 
+</div>
+<div id="footer">
+	<div class="info">
+		<p id="copyright">&copy; 2011 COSCUP. <a href="http://coscup.org/2011/contact">Contact us</a>.</p>
+		<p id="tagline">We <span class="heart">(heart)</span> Open.</p>
+		<p id="archives">
+			<a href="http://coscup.org/2006/">2006</a>
+			<span class="separator"> | </span><a href="http://coscup.org/2007/">2007</a>
+			<span class="separator"> | </span><a href="http://coscup.org/2008/">2008</a>
+			<span class="separator"> | </span><a href="http://coscup.org/2009/">2009</a>
+			<span class="separator"> | </span><a href="http://coscup.org/2010/">2010</a>
+		</p>
 	</div>
-
 </div>
 
-
-
-
-
-
-
-
-
-
-<?php print $closure; ?></body></html>
+<?php print $closure; ?>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script type="text/javascript" src="http://coscup.org/2011-theme/assets/jquery.imagetile.js"></script>
+<script type="text/javascript" src="http://coscup.org/2011-theme/assets/script.js"></script>
+</body></html>
