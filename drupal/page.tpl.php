2e30cb2cc359db0b3adda38774f9a5751f6808e9
Timothy Guan-tin Chien <timdream@gmail.com>
2011-04-17 19:34:59 -0700

    Add multiple sub-domain GA code to sponsor.coscup.org (not sure, experimenting)

--- drupal/page.tpl.php
+++ drupal/page.tpl.php
@@ -12,6 +12,20 @@
 <link type="text/css" rel="stylesheet" media="all" href="http://coscup.org/2011-theme/assets/style.css" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta property="og:image" content="http://coscup.org/2011-theme/assets/coscup.png" />
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12923351-1']);
+  _gaq.push(['_setDomainName', '.coscup.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 
 <body class="<?php print $body_classes; ?>">
