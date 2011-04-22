c0b38de68b5af04705790d590c82b16a7d6730b2
Timothy Guan-tin Chien <timdream@gmail.com>
2011-04-22 11:06:30 +0800

    Remove sub-domain GA from sponsor.coscup.org (it confuses GA report)

--- drupal/page.tpl.php
+++ drupal/page.tpl.php
@@ -14,20 +14,6 @@
 <!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="http://coscup.org/2011-theme/assets/style.css" media="screen"/><![endif]-->
 <meta name="viewport" content="width=device-width" />
 <meta property="og:image" content="http://coscup.org/2011-theme/assets/coscup.png" />
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-12923351-1']);
-  _gaq.push(['_setDomainName', '.coscup.org']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </head>
 
 <body class="<?php print $body_classes; ?>">
