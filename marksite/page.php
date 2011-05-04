e578d9763d5595f9285c2315d44450c5bc6bf6e0
Xaver Y.R. Chen <yrchen@ATCity.org>
2011-05-04 19:08:08 +0800

    Specify a startup image for mobile safari

--- marksite/page.php
+++ marksite/page.php
@@ -33,7 +33,7 @@ switch($this->current[0])
 	<link rel="apple-touch-icon" href="<?php echo $theme_assets_uri;?>coscup-icon-iphone.png" />
 	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $theme_assets_uri;?>coscup-icon-ipad.png" />
 	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $theme_assets_uri;?>coscup-icon-iphone4.png" />
-
+	<link rel="apple-touch-startup-image" href="<?php echo $theme_assets_uri;?>coscup-startup.png" />
 	<script type="text/javascript">
 
 	  var _gaq = _gaq || [];
