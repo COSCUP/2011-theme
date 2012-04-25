0e8ba7c56f0d213defc5488f0976be1b29341c05
Timothy Guan-tin Chien <timdream@gmail.com>
2012-04-26 01:39:18 +0800

    Update appcacheui.js

--- assets/script.js
+++ assets/script.js
@@ -17,8 +17,8 @@
 */
 
 /* AppCacheUI: https://github.com/timdream/appcacheui */
-(function(){var e={init:function(){var a=document,b=a.body,c=window.applicationCache;if(c)if(b){this.info=a.getElementById("appcache_info");if(!this.info){a.cE=a.createElement;var d=a.cE("a"),a=a.cE("div");a.id="appcache_info";d.href="";a.appendChild(d);b.firstChild&&b.insertBefore(a,b.firstChild);this.info=a}"checking,downloading,progress,noupdate,cached,updateready,obsolete,error".split(",").forEach(function(a){c.addEventListener(a,e)})}else console.log("Premature init. Put the <script> in <body>.")},
-handleEvent:function(a){this.info.className=this.info.className.replace(/ ?appcache\-.+\b/g,"")+" appcache-"+a.type;"progress"===a.type&&a.total&&this.info.setAttribute("data-progress",a.loaded+1+"/"+a.total)}};e.init()})();
+(function(){var e={b:function(){var a=document,b=a.body,d=window.applicationCache;if(d)if(b){this.info=a.getElementById("appcache_info");if(!this.info){a.a=a.createElement;var c=a.a("a"),a=a.a("div");a.id="appcache_info";c.href="";c.addEventListener("click",function(a){a.stopPropagation();window.location.reload()},!0);a.appendChild(c);b.firstChild&&b.insertBefore(a,b.firstChild);this.info=a}"checking,downloading,progress,noupdate,cached,updateready,obsolete,error".split(",").forEach(function(a){d.addEventListener(a,
+e)})}else console.log("Premature init. Put the <script> in <body>.")},handleEvent:function(a){this.info.className=this.info.className.replace(/ ?appcache\-.+\b/g,"")+" appcache-"+a.type;"downloading"===a.type&&(this.count=0);"progress"===a.type&&(this.count++,this.info.setAttribute("data-progress",a.total?a.loaded+1+"/"+a.total:this.count))}};e.b()})();
 
 jQuery(function ($) {
 
